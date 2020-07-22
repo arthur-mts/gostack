6d2d959432435159197b77b1f34737923239d271
Arthur Mauricio <tuca@arch.localdomain>
2020-07-22 13:01:09 -0300

    add cors to backend

--- gobarber/backend/src/server.ts
+++ gobarber/backend/src/server.ts
@@ -2,6 +2,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -13,6 +14,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(routes);
 
